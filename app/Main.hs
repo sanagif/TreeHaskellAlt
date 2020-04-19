d8a4ade5c4a07c989b463bd8ce016a3c3cfd0e92
sanagif <sanagif@vivaldi.net>
2020-04-19 23:38:36 +0900

    [Add] Example code for command line argument parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,42 @@
 module Main where
 
 import Lib
+import System.Console.ArgParser
+import Control.Applicative
+{-
+data MyTest = MyTest Int Int
+
+myTestParser :: ParserSpec MyTest
+myTestParser = MyTest
+  `parsedBy` reqPos "pos1" `Descr` "description for the first argument"
+  `andBy` optPos 0 "pos2" `Descr` "description"
+
+myTestInterface :: IO (CmdLnInterface MyTest)
+myTestInterface =
+  (`setAppDescr` "top description")
+  <$> (`setAppEpilog` "bottom decription")
+  <$> mkApp myTestParser
 
 main :: IO ()
-main = someFunc
+main = do
+  interface <- myTestInterface
+  runApp interface print -}
+
+
+data MyTest = MyTest Int Int
+  deriving (Show) -- we will print the values
+
+myTestParser :: ParserSpec MyTest
+myTestParser = MyTest
+  `parsedBy` reqPos "pos1" `Descr` "description for the first argument"
+  `andBy` optPos 0 "pos2" `Descr` "description for the second argument"
+
+myTestInterface :: IO (CmdLnInterface MyTest)
+myTestInterface =
+  (`setAppDescr` "top description")
+  <$> (`setAppEpilog` "bottom description")
+  <$> mkApp myTestParser
+
+main = do
+  interface <- myTestInterface
+  runApp interface print
